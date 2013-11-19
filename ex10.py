10d737c6b815c7375f67766b710246cafee79038
Arthur <travic@travic-VPCEA25FX.(none)>
2013-11-19 12:11:21 -0500

    10/52

---
+++ ex10.py
@@ -0,0 +1,17 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "i'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
+
+#\t is to make a tab and \n is to make a new line \\ adds \ to what your typing.
