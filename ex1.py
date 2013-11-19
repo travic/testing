ab1485ca2043ab849990e8f0a6417c749cb47c5f
Arthur <travic@travic-VPCEA25FX.(none)>
2013-11-19 11:36:52 -0500

    my first python program from the book

---
+++ ex1.py
@@ -0,0 +1,8 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.' #<-doesnt use a " is using ' on purpose
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+#this line wont print ^_^
