a0c745d7e4e85a4bf5f5a12dd792b22faebfdd03
Arthur <travic@travic-VPCEA25FX.(none)>
2013-11-20 17:15:34 -0500

    34! Added a bit of my own comments to it for fun.

---
+++ ex34.py
@@ -0,0 +1,49 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+one = animals[1]
+print "The animal at 1 is %s." % one
+
+two = animals[2]
+print "The 3rd animal is %s." % two
+
+three = animals[0]
+print "The 1st animal is %s." % three
+
+four = animals[3]
+print "The animal at 3 is %s." % four
+
+five = animals[4]
+print "The 5th animal is %s." % five
+
+six = animals[2]
+print "The animal at 2 is %s." % six
+
+seventh = animals[5]
+print "The 6th animals is %s." % seventh
+
+eight = animals[4]
+print "The animal at 4 is %s." %eight
+
+#This exercise is a bit of a pain in the ass lets try it again
+
+print ""
+print "Lets talk about GP:DC for a minute."
+decks = ['Death and Taxes', 'Dredge', 'Sneak and Show', 'Elves', 'UWR Delver', 'Shardless Bug', 'Esper Stoneblade', 'Bant']
+
+d1 = decks[2]
+d2 = decks[4]
+print "The finals consisted of the following two decks %s and %s." % (d1, d2)
+
+d3 = decks[4]
+print "In the end %s was crowned the victor." % d3
+
+d4 = decks[0] # Death and Taxes
+d5 = decks[3] # Elves
+d6 = decks[7] # Bant
+d8 = decks[2] # Sneak and Show
+d9 = decks[4] # UWR Delver
+d10 = decks[1] # Dredge
+print "So Wei ended up playing %s and now wishes he played a blue deck along the lines of %s." % (d4, d6)
+print "Pat did really well and lost his win and in but complains he didnt play %s." % d10
+print "I know I played a rogue deck and now I am going to be playing %s but I know I wont do as good as Jared." % d8
+print "Wei's brother ended up playing %s and cries now whenever he hears the word TOP." % d5
